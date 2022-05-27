5d07a620ff87b0bdf0a16da8b12116ced4ce67c2
walakao <walawalawalatest@126.com>
2022-05-28 07:40:28 +0800

    初始化

---
+++ src/com/demo/Demo.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+/**
+ * @Auther: LX Chen
+ * @Date: 2022/5/28 - 05 - 28 - 07:37
+ * @version: 1.0
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("项目开始了！");
+    }
+}
