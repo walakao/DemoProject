0ec595915e0b2c699d0da95f15f2e26650965685
globalwalakao <walawalawalatest@126.com>
2022-05-28 16:32:20 +0800

    创建一点类

---
+++ src/dao/PersonDao.java
@@ -0,0 +1,9 @@
+package dao;
+
+/**
+ * @Auther: LX Chen
+ * @Date: 2022/5/28 - 05 - 28 - 16:30
+ * @version: 1.0
+ */
+public interface PersonDao {
+}
---
+++ src/pojo/Person.java
@@ -0,0 +1,9 @@
+package pojo;
+
+/**
+ * @Auther: LX Chen
+ * @Date: 2022/5/28 - 05 - 28 - 16:29
+ * @version: 1.0
+ */
+public class Person {
+}
